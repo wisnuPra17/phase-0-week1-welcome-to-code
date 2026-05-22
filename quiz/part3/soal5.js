932ead765ec10183d28417fe204e2f2e751ac39a
wisnuPra17 <dewaw72@gmail.com>
2026-05-22 09:25:58 +0800

    Edit massage in README.md and Add file soal5.js

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,16 @@
+// Problem buatlah bintang seperti berikut
+let input = 5;
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+let star = "";
+for (let i = 0; i< 5; i++) {   
+    for (let j = 0; j <= i; j++) {  
+        star += "*";
+    }
+    star += "\n";
+}
+console.log(star);
